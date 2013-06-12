c07dc210ce7ab26db225b48189fc5cfe5c1a98fc
Morgan Bonar <mbonar@gmail.com>
2013-06-11 22:40:32 -0700

    Major revisions

--- characterFetch/app.js
+++ characterFetch/app.js
@@ -1,154 +1,150 @@
 /**App Launcher*/
+
+//Create a top level global object to store our models/veiws/configs/etc.
+appSettings = {models: {}, views: {}, dataManipulators: {}};
+appSettings.char_id = '5428013610387904065';
+appSettings.censusURL = 'https://census.soe.com/get/';
+appSettings.censusGame = 'ps2';
+
+
+//Define the charModel
+appSettings.models.CharacterModel = Backbone.Model.extend({
+	initialize: function(){
+		// this.resolves = '?c:resolve=friends,stat,stat_history,weapon_stat,item_full(type),world,outfit(alias,id,name),online_status&c:internal=true';
+		// this.resolves = '?c:internal=true'
+	},
+
+	url: function(){
+		return appSettings.censusURL 
+		+ appSettings.censusGame + '/'
+		+ 'single_character_by_id/?id='
+		+ appSettings.char_id 
+	},
+	sync: function(method, model, options){
+		var params = _.extend({
+			type: 'GET',
+			dataType: 'jsonp',
+			url: model.url(),
+			processData: false,
+			cache: true
+		}, options)
+
+		return Backbone.sync(method, model, params);
+	},
+	parse: function(response){
+		var rawchardata = response.single_character_by_id_list[0];
+		
+		console.log(rawchardata);
+		console.log(appSettings.dataManipulators.vehicleStats(rawchardata.weapon_stats));
+		console.log(appSettings.dataManipulators.itemStats(rawchardata.weapon_stats));
+		console.log(appSettings.dataManipulators.characterStats(rawchardata.stats));
+
+		return rawchardata;
+	}
+});
+
+
+appSettings.dataManipulators.vehicleStats = function(weaponStats){
+	/**
+	This data manipulator is expecting to get an unfiltered list of item stats(character_list[0].weapon_stats) via the c:resolve=weapon_stat paramater on the charcter data URL request
+	It converts the raw list of stats into indexed lists for both vehicles
+	*/		
+
+	/**Create a list of items and their stats by group*/
+	var groupedItemsList = _.groupBy(weaponStats, 'item_id');
+	//At this point we have an indexed list of all the item ids; the '0' object is the list of vehicles' stats
+
+
+	/**Create a master list of the Vehicle Stats (no weapons involved) */
+	var masterVehicleStatsByID = {};
+	var vehicleprefix = 'v_';
+	var groupedVehiclesList = _.groupBy(groupedItemsList[0], 'vehicle_id');
+	_.each(groupedVehiclesList, function(vehicle, key, list){
+		masterVehicleStatsByID[vehicleprefix+key] = {'id': key};
+		_.each(vehicle, function(stat){
+			masterVehicleStatsByID[vehicleprefix+key][stat.stat_name] = {'value': stat.value, 'last_save': stat.last_save};
+		});
+	});
+	//add the vehicle name if it exists in appSettings.vehicleTable
+	if(appSettings.vehicleTable) {
+		_.each(masterVehicleStatsByID, function(vehicleRecord){
+			if(appSettings.vehicleTable[vehicleRecord.id])	vehicleRecord['name'] = appSettings.vehicleTable[vehicleRecord.id].name.en;
+		});
+	}
+
+	return masterVehicleStatsByID;
+
+};
+
+appSettings.dataManipulators.itemStats = function(weaponStats){
+	/**
+	This data manipulator is expecting to get an unfiltered list of item stats(character_list[0].weapon_stats) via the c:resolve=weapon_stat paramater on the charcter data URL request
+	It converts the raw list of stats into indexed lists for items
+	*/
+
+	/**Create a list of items and their stats by group*/
+	var groupedItemsList = _.groupBy(weaponStats, 'item_id');
+	//At this point we have an indexed list of all the item ids; the '0' object is the list of vehicles' stats
+	
+	/**Create the master list of items, with the vehicles removed*/
+	var masterItemStatsByID = {};
+	var weaponprefix = 'i_';
+	delete groupedItemsList[0]; //remove the vehicle stats; not needed for itemslist
+
+	_.each(groupedItemsList, function(item, key, list){
+		masterItemStatsByID[weaponprefix+key] = {'id': key};
+		_.each(item, function(stat){
+			masterItemStatsByID[weaponprefix+key][stat.stat_name] = {'value': stat.value, 'last_save': stat.last_save};
+		});
+	});
+	//add the item name if it exists in appSettings.itemsTable
+	if(appSettings.itemsTable) {
+		_.each(masterItemStatsByID, function(itemRecord){
+			if(appSettings.itemsTable['item_'+itemRecord.id]) itemRecord['name'] = appSettings.itemsTable['item_'+itemRecord.id].name.en;
+		});
+	}
+	return masterItemStatsByID;
+
+};
+
+appSettings.dataManipulators.characterStats = function(charStats){
+	var groupedProfileList = _.groupBy(charStats, 'profile_id');
+
+	var masterCharStatsByProfile = {};
+	var profileprefix = 'p_';
+
+	_.each(groupedProfileList, function(profile, key, list){
+		masterCharStatsByProfile[profileprefix+key] = {'id': key};
+		_.each(profile, function(stat){
+			masterCharStatsByProfile[profileprefix+key][stat.stat_name] = stat;
+		});
+	});
+	
+	return masterCharStatsByProfile;
+};
+
+
+//Define the CharView
+appSettings.views.CharView = Backbone.View.extend({
+	attributes: {
+		'class': 'character'
+	},
+	initialize: function(options){
+		this.template = _.template($('#tpl-character').html());
+	},
+	render: function(){
+		var $this = this.$el;
+		$this.html(this.template(this.model.toJSON()));
+		return $this;
+	}
+});
+
+//Start up a Backbone model to grab the data
+appSettings.charModel = new appSettings.models.CharacterModel();
+
 $(function(){
 	//DOM Ready
 	console.log('app started')
-
-	//Create a top level global object to store our models/veiws/configs/etc.
-	appSettings = {models: {}, views: {}, dataManipulators: {}};
-	appSettings.char_id = '5428013610387904065';
-	appSettings.censusURL = 'https://census.soe.com/get/';
-	appSettings.censusGame = 'ps2';
-	// appSettings.censusGame = 'ps2-beta';
-
-
-	//Define the charModel
-	appSettings.models.CharacterModel = Backbone.Model.extend({
-		initialize: function(){
-			this.resolves = '?c:resolve=friends,stat,stat_history,weapon_stat,item_full(type),world,outfit(alias,id,name),online_status&c:internal=true';
-			// this.resolves = '?c:internal=true'
-		},
-
-		url: function(){
-			return appSettings.censusURL 
-			+ appSettings.censusGame + '/'
-			+ 'character/'
-			+ appSettings.char_id 
-			+ this.resolves
-		},
-		sync: function(method, model, options){
-			var params = _.extend({
-				type: 'GET',
-				dataType: 'jsonp',
-				url: model.url(),
-				processData: false,
-				cache: true
-			}, options)
-
-			return Backbone.sync(method, model, params);
-		},
-		parse: function(response){
-			var rawchardata = response.character_list[0];
-			
-			console.log(rawchardata);
-			console.log(appSettings.dataManipulators.vehicleStats(rawchardata.weapon_stats));
-			console.log(appSettings.dataManipulators.itemStats(rawchardata.weapon_stats));
-			console.log(appSettings.dataManipulators.characterStats(rawchardata.stats));
-
-
-
-
-			return rawchardata;
-		}
-	});
-
-
-	appSettings.dataManipulators.vehicleStats = function(weaponStats){
-		/**
-		This data manipulator is expecting to get an unfiltered list of item stats(character_list[0].weapon_stats) via the c:resolve=weapon_stat paramater on the charcter data URL request
-		It converts the raw list of stats into indexed lists for both vehicles
-		*/		
-
-		/**Create a list of items and their stats by group*/
-		var groupedItemsList = _.groupBy(weaponStats, 'item_id');
-		//At this point we have an indexed list of all the item ids; the '0' object is the list of vehicles' stats
-
-
-		/**Create a master list of the Vehicle Stats (no weapons involved) */
-		var masterVehicleStatsByID = {};
-		var vehicleprefix = 'v_';
-		var groupedVehiclesList = _.groupBy(groupedItemsList[0], 'vehicle_id');
-		_.each(groupedVehiclesList, function(vehicle, key, list){
-			masterVehicleStatsByID[vehicleprefix+key] = {'id': key};
-			_.each(vehicle, function(stat){
-				masterVehicleStatsByID[vehicleprefix+key][stat.stat_name] = {'value': stat.value, 'last_save': stat.last_save};
-			});
-		});
-		//add the vehicle name if it exists in appSettings.vehicleTable
-		if(appSettings.vehicleTable) {
-			_.each(masterVehicleStatsByID, function(vehicleRecord){
-				if(appSettings.vehicleTable[vehicleRecord.id])	vehicleRecord['name'] = appSettings.vehicleTable[vehicleRecord.id].name.en;
-			});
-		}
-
-		return masterVehicleStatsByID;
-
-	};
-
-	appSettings.dataManipulators.itemStats = function(weaponStats){
-		/**
-		This data manipulator is expecting to get an unfiltered list of item stats(character_list[0].weapon_stats) via the c:resolve=weapon_stat paramater on the charcter data URL request
-		It converts the raw list of stats into indexed lists for items
-		*/
-
-		/**Create a list of items and their stats by group*/
-		var groupedItemsList = _.groupBy(weaponStats, 'item_id');
-		//At this point we have an indexed list of all the item ids; the '0' object is the list of vehicles' stats
-		
-		/**Create the master list of items, with the vehicles removed*/
-		var masterItemStatsByID = {};
-		var weaponprefix = 'i_';
-		delete groupedItemsList[0]; //remove the vehicle stats; not needed for itemslist
-
-		_.each(groupedItemsList, function(item, key, list){
-			masterItemStatsByID[weaponprefix+key] = {'id': key};
-			_.each(item, function(stat){
-				masterItemStatsByID[weaponprefix+key][stat.stat_name] = {'value': stat.value, 'last_save': stat.last_save};
-			});
-		});
-		//add the item name if it exists in appSettings.itemsTable
-		if(appSettings.itemsTable) {
-			_.each(masterItemStatsByID, function(itemRecord){
-				if(appSettings.itemsTable['item_'+itemRecord.id]) itemRecord['name'] = appSettings.itemsTable['item_'+itemRecord.id].name.en;
-			});
-		}
-		return masterItemStatsByID;
-
-	},
-
-	appSettings.dataManipulators.characterStats = function(charStats){
-		var groupedProfileList = _.groupBy(charStats, 'profile_id');
-
-		var masterCharStatsByProfile = {};
-		var profileprefix = 'p_';
-
-		_.each(groupedProfileList, function(profile, key, list){
-			masterCharStatsByProfile[profileprefix+key] = {'id': key};
-			_.each(profile, function(stat){
-				masterCharStatsByProfile[profileprefix+key][stat.stat_name] = stat;
-			});
-		});
-		
-		return masterCharStatsByProfile;
-	},
-
-
-	//Define the CharView
-	appSettings.views.CharView = Backbone.View.extend({
-		attributes: {
-			'class': 'character'
-		},
-		initialize: function(options){
-			this.template = _.template($('#tpl-character').html());
-		},
-		render: function(){
-			var $this = this.$el;
-			$this.html(this.template(this.model.toJSON()));
-			return $this;
-		}
-	});
-
-	//Start up a Backbone model to grab the data
-	appSettings.charModel = new appSettings.models.CharacterModel();
 
 	appSettings.charModel.fetch({
 		success: function(){
@@ -157,7 +153,6 @@ $(function(){
 			appSettings.charView = new appSettings.views.CharView({model: appSettings.charModel});
 			$('#main-container').html(appSettings.charView.render());
 		},
-	});
-
-	
+	});	
 });
+
--- characterFetch/bookmarklet.js
+++ characterFetch/bookmarklet.js
@@ -1,59 +1,128 @@
-if(typeof jQuery != 'undefined'){
-	console.log(jQuery().jquery);
-}
-console.log(ps2char_id);
-var mywidget = document.getElementById("my_unique_id");
 
-if(mywidget){
-	mywidget.parentNode.removeChild(mywidget);
-} else {
+PS2CharWidget = {
+    myWidget: null,
 
-var myCSS, 
-	myStyleNode,
-    myHTML, 
-    myHTMLNode,
-   	sayHello;
- 
-    /* add the css */
-    myCSS  = '#my_unique_id, #my_unique_id *{font-family:Courier,"Courier New",sans-serif;color:#333;line-height:1.5em;font-size:15px;margin:0;padding:0;text-shadow:none;}';
-    myCSS += '#my_unique_id {z-index:10000;position:fixed;top:0;right:300px;background-color:#ccc;}';
-    myCSS += '#my_unique_id .c {width:200px; padding:20px;}';
-    myCSS += '#my_unique_id h1 {font-size:20px; margin-bottom:0.5em;color:#0080C0}';
-    myCSS += '#my_unique_id p {margin-bottom:0.5em;}';
-    /* then insert it */
-    myStyleNode =  document.createElement('style');
-    myStyleNode.innerHTML = myCSS;
-    document.body.appendChild(myStyleNode);
- 
-    /* build the HTML element */
-    myHTML  = '<div class="c">';
-    myHTML += '<h1>Boring Widget</h1>';
-    myHTML += '<p>That says hello when you click it.</p>';
-    myHTML += '</div>';
-    myHTML += '<div>';
-    myHTML += document.cookie;
-    myHTML += '</div>';
-    /* and create the node */
-    myHTMLNode = document.createElement('div');
-    myHTMLNode.id = 'my_unique_id';
-    myHTMLNode.innerHTML = myHTML;
- 
-    /* add js functionality to it */
-    sayHello = function() {
-      var who = window.prompt('who?'); alert('hello '+who);
-    };
- 
-    if (myHTMLNode.addEventListener) {
-      myHTMLNode.addEventListener('click', sayHello, false);
-    } else if (el.attachEvent) {
-      myHTMLNode.attachEvent('onclick', sayHello);
+    initBookmarklet: function(){
+        _this = this;
+
+        if(this.myWidget){
+            this.myWidget.parentNode.removeChild(this.myWidget);
+            this.myWidget = null;
+        } else {
+
+            /* only put the CSS Styles on the page once */
+            if(!document.getElementById('PS2CharWidgetStyle')) {
+                var myCSS, 
+                    myStyleNode,
+                /* add the css */
+                myCSS  = '#my_unique_id, #my_unique_id *{font-family:Courier,"Courier New",sans-serif;color:#333;line-height:1.5em;font-size:15px;margin:0;padding:0;text-shadow:none;}';
+                myCSS += '#my_unique_id {overflow: hidden;width: 200px; height: 400px; z-index:10000;position:fixed;top:10px;right:0;background-color:#ccc;}';
+                myCSS += '#my_unique_id .c {}';
+                myCSS += '#my_unique_id h1 {font-size:20px; margin-bottom:0.5em;color:#0080C0}';
+                myCSS += '#my_unique_id p {margin-bottom:0.5em;}';
+                /* then insert it */
+                myStyleNode =  document.createElement('style');
+                myStyleNode.id = 'PS2CharWidgetStyle';
+                myStyleNode.innerHTML = myCSS;
+                document.body.appendChild(myStyleNode);
+            }
+
+            var myHTML, 
+                myHTMLNode;
+            /* build the HTML element */
+            myHTML  = '<div class="c">';
+            myHTML += '<h1>Boring Widget</h1>';
+            myHTML += '</div>';
+            /* and create the node */
+            myHTMLNode = document.createElement('div');
+            myHTMLNode.id = 'my_unique_id';
+            myHTMLNode.innerHTML = myHTML;
+         
+            /* add js functionality to it */
+            closeMe = function(){
+                _this.myWidget.parentNode.removeChild(_this.myWidget);
+                _this.myWidget = null;
+            };
+         
+            if (myHTMLNode.addEventListener) {
+              myHTMLNode.addEventListener('click', closeMe, false);
+            } else if (el.attachEvent) {
+              myHTMLNode.attachEvent('onclick', closeMe);
+            }
+         
+            /* to add MORE scripts */
+        //    var myScriptNode=document.createElement('script');
+        //    myScriptNode.setAttribute('src','http://some.js/file.more.js');
+        //    document.head.appendChild(myScriptNode);
+         
+            /* inject the node, with the event attached */
+            document.body.appendChild(myHTMLNode);
+            this.myWidget = document.getElementById("my_unique_id");
+
+            jQuery.ajax({
+                url: 'https://census.soe.com/get/ps2/single_character_by_id/?id=5428013610387904065',
+                dataType: 'jsonp'
+            }).done(function(data, textStatus, jqXHR) {
+                if(textStatus == 'success'){
+                    PS2CharWidget.loadChar(data);
+                } else {
+                    console.log('data error');
+                }
+            })
+        }
+    },
+
+    loadChar: function(response){
+        var chardata = response.single_character_by_id_list[0];
+        var frag = '<div>' + chardata.name.first + '</div>'
+
+        jQuery('#my_unique_id .c').html(frag);
     }
- 
-    /* to add MORE scripts */
-//    var myScriptNode=document.createElement('script');
-//    myScriptNode.setAttribute('src','http://some.js/file.more.js');
-//    document.head.appendChild(myScriptNode);
- 
-    /* inject the node, with the event attached */
-    document.body.appendChild(myHTMLNode);
 }
+
+// Only do anything if jQuery isn't defined
+if (typeof jQuery == 'undefined' || jqueryLessThan('1.8.0') ) {
+    function getScript(url, success) {    
+        var script = document.createElement('script');
+        script.src = url;
+        
+        var head = document.getElementsByTagName('head')[0],
+            done = false;
+        
+        // Attach handlers for all browsers
+        script.onload = script.onreadystatechange = function() {
+            if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+                done = true;                
+                // callback function provided as param
+                success();                
+                script.onload = script.onreadystatechange = null;
+                head.removeChild(script);                
+            };        
+        };        
+        head.appendChild(script);    
+    };
+    
+    getScript('http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function() {    
+            // jQuery loaded! Make sure to use .noConflict just in case
+            PS2CharWidget.initBookmarklet(); 
+    });
+    
+} else { // jQuery was already loaded
+    PS2CharWidget.initBookmarklet();
+}
+
+function jqueryLessThan(version){
+    if(typeof jQuery == 'undefined') return true;
+
+    var installedVersion = jQuery.fn.jquery.split('.');
+    var minVersion = version.split('.');
+
+    for(var i = 0, len = minVersion.length; i < len ; i++){
+        if(parseInt(installedVersion[i]) < parseInt(minVersion[i])){
+            return true;
+        }
+    }
+
+    return false;
+}
+
